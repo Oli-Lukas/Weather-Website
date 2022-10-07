21919b211fc9be7f9b2494fc55386333389794bf
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2022-10-07 00:59:48 -0300

    refactor: delete comments line

--- src/app.js
+++ src/app.js
@@ -14,8 +14,6 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 
