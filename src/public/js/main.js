61c30d0bdd21e0c1ac34e4560af03c1c5fb273d5
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2022-10-07 12:54:03 -0300

    feat: insert dinamically paragraphs titles
    modify website to insert dinamically paragraphs titles about cities
    names, conditions, etc.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -20,27 +20,51 @@ cityForm.addEventListener('submit', async (event) => {
 });
 
 function showCity(information) {
-  let tag_elements = {
+  let tagTitleElements = {
+    cityName   : document.querySelector("div.field.name > p.title"),
+    temperature: document.querySelector("div.field.temperature > p.title"),
+    datetime   : document.querySelector("div.field.datetime > p.title"),
+    conditions : document.querySelector("div.field.conditions > p.title")
+  }
+
+  tagTitleElements.cityName.innerHTML = '';
+  tagTitleElements.temperature.innerHTML = '';
+  tagTitleElements.datetime.innerHTML = '';
+  tagTitleElements.conditions.innerHTML = '';
+
+  let titlesTextNode = {
+    cityName   : document.createTextNode("City Name"),
+    temperature: document.createTextNode("Temperature"),
+    datetime   : document.createTextNode("Date & Time"),
+    conditions : document.createTextNode("Current Conditions")
+  };
+
+  tagTitleElements.cityName.appendChild(titlesTextNode.cityName);
+  tagTitleElements.temperature.appendChild(titlesTextNode.temperature);
+  tagTitleElements.datetime.appendChild(titlesTextNode.datetime);
+  tagTitleElements.conditions.appendChild(titlesTextNode.conditions);
+
+  let tagDataElements = {
     cityName   : document.querySelector("div.field.name > p.data"),
     temperature: document.querySelector("div.field.temperature > p.data"),
     datetime   : document.querySelector("div.field.datetime > p.data"),
     conditions : document.querySelector("div.field.conditions > p.data")
   };
 
-  tag_elements.cityName.innerHTML    = '';
-  tag_elements.temperature.innerHTML = '';
-  tag_elements.datetime.innerHTML    = '';
-  tag_elements.conditions.innerHTML  = '';
-
-  let textNodes = {
+  let dataTextNode = {
     cityName   : document.createTextNode(information.cityName),
     temperature: temperatureNode(information.Temperature.Metric.Value),
     datetime   : dateTimeNode(new Date(information.LocalObservationDateTime)),
     conditions : document.createTextNode(information.WeatherText)
   };
 
-  tag_elements.cityName.appendChild(textNodes.cityName);
-  tag_elements.temperature.appendChild(textNodes.temperature);
-  tag_elements.datetime.appendChild(textNodes.datetime);
-  tag_elements.conditions.appendChild(textNodes.conditions);
+  tagDataElements.cityName.innerHTML    = '';
+  tagDataElements.temperature.innerHTML = '';
+  tagDataElements.datetime.innerHTML    = '';
+  tagDataElements.conditions.innerHTML  = '';
+
+  tagDataElements.cityName.appendChild(dataTextNode.cityName);
+  tagDataElements.temperature.appendChild(dataTextNode.temperature);
+  tagDataElements.datetime.appendChild(dataTextNode.datetime);
+  tagDataElements.conditions.appendChild(dataTextNode.conditions);
 }
